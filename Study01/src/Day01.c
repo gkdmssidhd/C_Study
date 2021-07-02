1b238ac0807818782af45124351f1b6dafd94108
gkdmssidhd <75778395+gkdmssidhd@users.noreply.github.com>
2021-07-02 14:35:18 +0900

    07.02 commit

--- Study01/src/Day01.c
+++ Study01/src/Day01.c
@@ -2,15 +2,13 @@
 
 int main() {
 
-	int i = 250;
-	float a = 125.23f;
-	float b = 314.1592e+5;
-	char c ='A';
-	char dd[] = "Korea";
+	int a,b,c;
+	a = 5 % 3;
+	a--;
+	b = (a++) +3;
 
-	printf("10진수 i = %d\t 8진수i = %o\n", i , i);
-	printf("a = %8.2f, b = %e\n", a, b);
-	printf("c값은 문자로 %c이고 아스키 코드로 %d이다.\n", c, c);
-	printf("%-10s, %10s\n\r", dd, dd);
+	printf("%d, %d\n", a, b);
+	c = (++a) + 3;
+	printf("%d, %d, %d\n", a, b, c);
 
 }
